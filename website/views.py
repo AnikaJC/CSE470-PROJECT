5344d472604d9bc449865bdb998450162bcdd9a3
AnikaJC <anika.jahan.choudhury@g.bracu.ac.bd>
2022-08-12 19:50:59 +0600

    message

--- website/views.py
+++ website/views.py
@@ -22,3 +22,21 @@ def admin():
 
         return redirect(url_for('views.admin'))
     return render_template("admin.html",movies = all_data)
+
+@views.route('/update',methods =['POST'])
+def update():
+    
+    if request.method == 'POST':
+        movie_data = AddMovie.query.get(request.form.get('id'))
+        movie_data.movies_name = request.form.get('movies_name')
+        movie_data.movies_director = request.form.get('movies_director')
+        movie_data.genre = request.form.get('genre')
+
+        db.session.commit()
+        flash("Movie Successfully Updated!")
+        return redirect(url_for('views.admin'))
+    return render_template("admin.html")
+    
+
+
+
